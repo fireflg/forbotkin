795537edd8b190efc5663e6a0f772e617b1d9c40
fireflg <96112649+fireflg@users.noreply.github.com>
2022-02-13 08:49:04 -0800

    Add files via upload

---
+++ sqlpostgres.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+mysql -uroot -proot -h127.0.0.1 -P3307 --protocol=tcp <<MYSQL_SCRIPT
+CREATE DATABASE botkin;
+USE botkin; 
+CREATE TABLE COSTS(
+  id INT PRIMARY KEY AUTO_INCREMENT UNIQUE,
+  name VARCHAR(50) NOT NULL,
+  price INT NOT );
+ 
+CREATE TABLE PRODUCTS(
+  id INT  PRIMARY KEY AUTO_INCREMENT UNIQUE,
+  name VARCHAR(50) NOT NULL,
+  quantity INT,
+  priceid INT NOT NULL,
+  status VARCHAR(50) NOT NULL,
+  FOREIGN KEY (priceid)  REFERENCES COSTS (id));
+exit
+MYSQL_SCRIPT
+sleep 2
+PGPASSWORD=root psql -h 172.17.0.1 -p 5410 -U root -d root postgres -c 'CREATE DATABASE botkin' 
+PGPASSWORD=root psql -h 172.17.0.1 -p 5410 -U root -d botkin <<MYPOSTGRESSQL_SCRIPT
+CREATE TABLE COSTS(
+  id SERIAL PRIMARY KEY unique,
+  name VARCHAR(50) NOT NULL,
+  price INT NOT NULL);
+ 
+CREATE TABLE PRODUCTS(
+  id SERIAL PRIMARY KEY unique,
+  name VARCHAR(50) NOT NULL,
+  quantity INT NOT NULL,
+  priceid SERIAL,
+  status VARCHAR(50) NOT NULL,
+  FOREIGN KEY (priceid)  REFERENCES COSTS (id));
+
+
+
+
